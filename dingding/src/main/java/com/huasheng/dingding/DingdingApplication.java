d5752867d8a76c01341a5e056225080f5b860227
Administrator <951573889@qq.com>
2023-05-23 14:22:31 +0800

    '新增自动备份数据库功能'

--- dingding/src/main/java/com/huasheng/dingding/DingdingApplication.java
+++ dingding/src/main/java/com/huasheng/dingding/DingdingApplication.java
@@ -1,13 +1,18 @@
 package com.huasheng.dingding;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@Slf4j
 public class DingdingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DingdingApplication.class, args);
+        log.info("========================启动完毕========================");
     }
 
 }
---
+++ dingding/src/main/java/com/huasheng/dingding/scheduTask/BackUpSchedule.java
@@ -0,0 +1,22 @@
+package com.huasheng.dingding.scheduTask;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class BackUpSchedule {
+
+    @Scheduled(cron = "0 0 4 * * ?")
+    public void execute(){
+        try {
+            log.info("=======================开始备份数据库=========================");
+            Runtime.getRuntime().exec("cmd /c G:/HS-DDDK/20230523/backUp.bat"); // 执行备份批处理文件
+            log.info("=======================数据库备份成功=========================");
+        } catch (IOException e) {
+            log.error("数据库备份失败",e);
+        }
+    }
+}
